287601ff52e10d255103ba4b03df0f1d69e21b10
Aoyagi Kotaro <mehe2yagi@gmail.com>
2022-10-22 12:02:15 +0900

    text削除

--- static/js/common.js
+++ static/js/common.js
@@ -1,11 +1,5 @@
 $('.slide-items').slick({
-    asNavFor:"#text-slider",
     autoplay: true,
     autoplaySpeed: 3000,
     speed: 400
-});
-
-$('#text-slider').slick({
-    draggable:false,         //ドラッグでのスライド禁止
-    arrows:false             //矢印非表示
 });
